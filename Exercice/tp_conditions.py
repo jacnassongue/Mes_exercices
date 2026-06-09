480c0525f24cf7205029ef58b86734718fbaf2a4
gnassongue <jacnassongue26@gmail.com>
2026-06-09 23:57:08 +0000

    exercice 1 - 3

---
+++ Exercice/tp_conditions.py
@@ -0,0 +1,13 @@
+liste_1 = [14, 25, 28]
+nb = "10 est un nombre"
+print(10 in liste_1 and 10 in nb)
+# Exercice_2
+surface = 350
+zone_urbaine = True
+print((surface > 250) and (zone_urbaine ==True))
+
+#Exercice_3
+annees_depuis_releve = 8
+print((annees_depuis_releve >= 10) or ("cadastre" in "departement du cadastre"))
+
+# Exercice_4
