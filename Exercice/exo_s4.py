f2ecc1fe8ecbf560344f8b353985366fa099658a
gnassongue <jacnassongue26@gmail.com>
2026-06-21 17:22:24 +0000

    suites des exercices

--- Exercice/exo_s4.py
+++ Exercice/exo_s4.py
@@ -11,7 +11,7 @@ parcelles = ["PARC-001", "PARC-002", "PARC-003", "PARC-004", "PARC-005", "PARC-0
 surfaces = [350, 1200, 95, 640, 210, 1500] # en m²
 communes = ["Golfe 1", "Agoè-Nyivé 1", "Golfe 1", "Bas-Mono 1", "Agoè-Nyivé 1", "Bas-Mono 1"]
 # Question 1.1
-for i, parcelle in enumerate(parcelles, start = 1):
+# for i, parcelle in enumerate(parcelles, start = 1):
     # print(i, parcelle)
 # Question 1.2
 # for i, parcelle in enumerate(parcelles, start=1):
@@ -25,12 +25,17 @@ for i, parcelle in enumerate(parcelles, start = 1):
 # surfaces_total = sum(surfaces)
 # print(f"la surface totale est : {surfaces_total}")
 # # Le nombre de parcelles situées dans Golfe 1
-# nombre_parcelles_golfe_1 = communes.count("Golfe 1")
-# print(f"le nombre de parcelles dans la commune Golfe 1 est : {nombre_parcelles_golfe_1}")
+# nombre_parcelles_golfe_1 = 0
+# # print(f"le nombre de parcelles dans la commune Golfe 1 est : {nombre_parcelles_golfe_1}")
+# for i, parcelle in enumerate(parcelles):
+#     if communes[i] == "Golfe 1":
+#         nombre_parcelles_golfe_1 = nombre_parcelles_golfe_1 +1
+# print(f'le nombre de parcelle dans Golfe 1 est : {nombre_parcelles_golfe_1}')
+
 # # les étiquettes de classeurs
 # for i in range(len(parcelles)):
 #     print(f'Etiquette n°{i+1}')
-# # Question 1.6-La recherche au guichet (while + break)
+# Question 1.6-La recherche au guichet (while + break)
 # while True:
 #     id_parcelle = input("Entrer l'identifiant de votre parcelle :")
 #     if id_parcelle in parcelles:
@@ -41,9 +46,15 @@ for i, parcelle in enumerate(parcelles, start = 1):
 #         print("Fermeture du guichet")
 #         break
 # Exercie 2-Le suivi des demandes de permis de construire
-    dossiers = ["DOS-201","DOS-202", "DOS-203", "DOS-204", "DOS-205"]
-    demandeurs = ["Atta Koffi", "Mireille Yao", "Ibrahim Dosso", "Fatou Coulibaly", "Ernest Bah"]
-    statuts    = ["approuvé", "en_attente", "rejeté", "en_attente", "approuvé"]
-# Question2.1
+dossiers = ["DOS-201","DOS-202", "DOS-203", "DOS-204", "DOS-205"]
+demandeurs = ["Atta Koffi", "Mireille Yao", "Ibrahim Dosso", "Fatou Coulibaly", "Ernest Bah"]
+statuts    = ["approuvé", "en_attente", "rejeté", "en_attente", "approuvé"]
+# # Question2.1
 for i, (dossier, demandeur) in enumerate(zip(dossiers, demandeurs)):
     print(f'{i+1}. {dossier} - {demandeur}')
+for i, dossier in enumerate(dossiers):
+    print(f"{i+1}. {dossier} - {demandeurs[i]} avec statut : {statuts[i]}")
+# liste_2 = [1, 2, 3, 4, 5, 6, 7]
+# liste_3 = ["un", "deux", "trois", "quatre"]
+# for a, b in enumerate(liste_3):
+#     print(f"{liste_2[a]} = {b}")
